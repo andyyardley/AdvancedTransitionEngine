29a22d70fa232f6065018c2a863f512077e16708
Andy Yardley <andy@niveusrosea.com>
2015-10-05 17:38:53 +0100

    Initial Commit

---
+++ Advanced Transition Engine/Advanced_Transition_Engine.h
@@ -0,0 +1,13 @@
+//
+//  Advanced_Transition_Engine.h
+//  Advanced Transition Engine
+//
+//  Created by Andy on 05/10/2015.
+//  Copyright © 2015 niveusrosea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Advanced_Transition_Engine : NSObject
+
+@end
---
+++ Advanced Transition Engine/Advanced_Transition_Engine.m
@@ -0,0 +1,13 @@
+//
+//  Advanced_Transition_Engine.m
+//  Advanced Transition Engine
+//
+//  Created by Andy on 05/10/2015.
+//  Copyright © 2015 niveusrosea. All rights reserved.
+//
+
+#import "Advanced_Transition_Engine.h"
+
+@implementation Advanced_Transition_Engine
+
+@end
